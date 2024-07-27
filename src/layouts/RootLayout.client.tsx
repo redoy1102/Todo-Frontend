e0d172d8ff93e18c2fa131ee6c5f150cd4950d20
Babul Akter <babulakterfsd@gmail.com>
2024-07-27 09:05:39 +0600

    dark mode removed

--- src/layouts/RootLayout.client.tsx
+++ src/layouts/RootLayout.client.tsx
@@ -1,23 +1,12 @@
 'use client';
 
-import DarkModeSwitcher from '@/components/shared/DarkModeSwitcher';
 import Providers from '@/lib/providers';
 import { Toaster } from 'sonner';
 
 const RootLayoutClient = ({ session, roboto, children }: any) => {
-  const toggleDarkMode = (mode: string) => {
-    const html = document.getElementById('mainhtml');
-    if (mode) {
-      html?.classList.add('dark');
-    } else {
-      html?.classList.remove('dark');
-    }
-  };
-
   return (
-    <html lang="en" id="mainhtml">
+    <html lang="en">
       <body className={roboto.className}>
-        <DarkModeSwitcher toggleDarkMode={toggleDarkMode} />
         <Toaster />
         <Providers session={session}>
           <div className="min-h-screen">{children}</div>
