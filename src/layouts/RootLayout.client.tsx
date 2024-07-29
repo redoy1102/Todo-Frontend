4a010a35d43d46dbcdcb75c8110db8c9d658c1c1
Babul Akter <babulakterfsd@gmail.com>
2024-07-29 11:50:42 +0600

    toaster and aos setup done

--- src/layouts/RootLayout.client.tsx
+++ src/layouts/RootLayout.client.tsx
@@ -5,14 +5,12 @@ import Providers from '@/providers/Providers';
 
 const RootLayoutClient = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <Providers>
-        <div className="absolute bottom-0 left-1 z-50">
-          <ThemeToggle />
-        </div>
-        <main>{children}</main>
-      </Providers>
-    </>
+    <Providers>
+      <div className="absolute bottom-0 left-1 z-50">
+        <ThemeToggle />
+      </div>
+      <main>{children}</main>
+    </Providers>
   );
 };
 
