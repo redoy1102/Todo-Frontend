57c1b5fed8c6f8faecbf808357dab83804dbc84d
Babul Akter <babulakterfsd@gmail.com>
2024-07-31 21:09:29 +0600

    framer motion added

--- src/components/clients/Homepage.client.tsx
+++ src/components/clients/Homepage.client.tsx
@@ -2,8 +2,11 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const HomepageClient = () => {
+  const [content, setContent] = useState('Welcome to the Nextjs RND.');
+
   return (
     <div className="flex flex-col gap-y-4 justify-center items-center ">
       <div className="w-full md:w-10/12 lg:w-8/12 mx-auto">
@@ -23,8 +26,12 @@ const HomepageClient = () => {
         initial={{ skewY: 10, y: -10 }}
         animate={{ skewY: 0, y: 0 }}
         transition={{ type: 'spring', duration: 2 }}
+        drag={true}
+        whileDrag={{ color: 'red' }}
+        onDragStart={() => setContent('টানাটানি কইরেন না')}
+        onDragEnd={() => setContent('Welcome to the Nextjs RND.')}
       >
-        Welcome to the Nextjs RND.
+        {content}
       </motion.h1>
       <p className="text-md w-full md:w-10/12 lg:w-8/12 mx-auto text-dim">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum nihil
